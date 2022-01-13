e9424261c11cb7d520e5b2c51877697737526b81
PHJ <haojie02074@gmail.com>
2022-01-13 17:44:59 +0800

    Complete CRUD

---
+++ src/components/ContactDetail.js
@@ -0,0 +1,35 @@
+import React from "react";
+import defaultImg from "../images/user.png"
+import { Link, useLocation } from "react-router-dom";
+
+const ContactDetail = (props) => {
+
+    const location = useLocation();
+
+    const { name, email } = location.state.contact;
+
+    return (
+
+        <div className="main">
+            <div className="ui card centered">
+                <div className="image">
+                    <img src={defaultImg} alt="user" />
+                </div>
+                <div className="content">
+                    <div className="header">{name}</div>
+                    <div className="description">{email}</div>
+                </div>
+            </div>
+            <div className="center-div">
+                <Link to="/">
+                    <button className="ui button blue centered">Back to Contact List</button>
+                </Link>
+
+            </div>
+        </div>
+
+    );
+}
+
+
+export default ContactDetail;
